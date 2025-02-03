57a3f57bb1bd96efc75c3c9d97e6d80cd7143efe
Tannur0606 <tannurka0606@gmail.com>
2025-02-03 17:42:59 +0500

    Delete UserFactory.java

--- UserFactory.java
+++
@@ -1,9 +0,0 @@
-public class UserFactory {
-    public static User createUser(String role, int id, String name, String email, String password) {
-        return switch (role) {
-            case "Admin" -> new Admin(id, name, email, password);
-            case "Customer" -> new Customer(id, name, email, password);
-            default -> throw new IllegalArgumentException("Invalid user role");
-        };
-    }
-}
